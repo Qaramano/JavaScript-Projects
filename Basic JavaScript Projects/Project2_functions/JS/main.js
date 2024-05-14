b601591d5bdd8db479f32662056a8db1a06fbe6b
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-05-14 17:16:47 -0600

    using the += operator

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//fnction that concatenate using +=
+function myFunction() {
+    var example = "I'm a web developer"; //assign the var
+    example += "still a long road to reach the my goal!"; //now using the += operator to add to the var
+    document.getElementById("Concatenate").innerHTML = example; //using the document.getelemntbyid
+}
