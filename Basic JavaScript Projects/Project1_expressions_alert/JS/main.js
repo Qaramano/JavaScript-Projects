886fc694edc20008436a96c22180b79505672f89
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-05-14 15:43:40 -0600

    created 2 vars and concatenate them, using an expression and commenting assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+window.alert ("Hello, World!") ;
+document.write("Hello, World!") ;
+
+var x = "My Name is Omar"
+window.alert(x); //using alert window
+
+
+document.write("Lisa told Bart, \"knock it off, Bart! or I'll tell dad!\"<br>\"Eat My shorts!\" Bart responded.");
+
+
+var B = ("\"be who you are and say what you want,"
+    +" because those who mind don\'t matter and those who matter don\'t mind.\""
+    +"-dr.seuss");
+    document.write(B) ; //concatanate two strings
+
+var Family = "the kramans", Dad = "omar", Mom = "Wedad", Daughter = "Elaf",
+    Daughter = "Nayrouz", Daughter = "Amira"; //assign multiuple values
+    document.write(Dad) ; //return a value
+
+    document.write(4+4); //using an expression
+
+    var sent1 = "This is the beginning of the string" //setting a string value to a varibale
+    var sent2 = "and this is the end of the string." //setting a string value to a varibale
+    document.write(sent1 + " " + sent2);
